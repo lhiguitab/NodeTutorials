fa50cf25e5b6418e2f03610b78fc3d8ef53ad8ac
lhiguitab <lhiguitab@eafit.edu.co>
2026-02-08 17:36:25 -0500

    Second Tutorial Completed

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,6 @@
 import type { Request, Response } from 'express';
+import { books } from '../data/books.js';
+import { Book } from '../models/Book.js';
 
 export class HomeController {
     static index(req: Request, res: Response): void {
@@ -18,7 +20,21 @@ export class HomeController {
     static contact(req: Request, res: Response): void {
         const viewData: { [key: string]: any } = {};
         viewData["title"] = "Contact";
-        
+
         res.render('home/contact', { viewData: viewData });
+    }
+
+    static Main_Point(req: Request, res: any) {
+        const viewData: any = {};
+        viewData["books"] = books;
+
+        res.render('home/books', viewData);
+    }
+
+    static show(req: any, res: any)
+    {
+        const book = Book.findById(books, parseInt(req.params.id));
+
+        res.render('home/show', { book: book })
     }
 }
